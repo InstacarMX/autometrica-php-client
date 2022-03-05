78899ccbf81cea51a40d408f52e855011c7ca8e2
Brandon Antonio Lorenzo <brandolin_a_@outlook.com>
2022-03-05 12:05:45 -0600

    Simplificación del código. - Se eliminó la clase proxy WebserviceHttpClient y se integró en AutometricaClient debido a que las clases estaban fuertemente acopladas. - Se eliminaron los test unitarios para WebserviceHttpClient.

--- src/AutometricaClient.php
+++ src/AutometricaClient.php
@@ -20,26 +20,59 @@
 
 namespace Instacar\AutometricaWebserviceClient;
 
+use Doctrine\Common\Annotations\AnnotationReader;
 use Instacar\AutometricaWebserviceClient\Exceptions\BadRequestHttpException;
 use Instacar\AutometricaWebserviceClient\Exceptions\UnauthorizedHttpException;
 use Instacar\AutometricaWebserviceClient\Exceptions\UnknownHttpException;
 use Instacar\AutometricaWebserviceClient\Model\Vehicle;
 use Instacar\AutometricaWebserviceClient\Model\VehiclePrice;
 use Instacar\AutometricaWebserviceClient\Response\CatalogResponse;
+use Instacar\AutometricaWebserviceClient\Response\CollectionResponseInterface;
 use Instacar\AutometricaWebserviceClient\Response\VehiclePricesResponse;
 use LogicException;
+use Nyholm\Psr7\Request;
 use Psr\Http\Client\ClientExceptionInterface;
 use Psr\Http\Client\ClientInterface;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpClient\Psr18Client;
+use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
+use Symfony\Component\Serializer\Mapping\Loader\AnnotationLoader;
+use Symfony\Component\Serializer\NameConverter\MetadataAwareNameConverter;
+use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class AutometricaClient
 {
-    private WebserviceHttpClient $webserviceClient;
+    private const BASE_URL = 'https://ws.autometrica.mx/';
+
+    private ClientInterface $client;
+
+    private SerializerInterface $serializer;
+
+    private string $username;
+
+    private string $password;
 
     public function __construct(ClientInterface $client, string $username, string $password)
     {
-        $this->webserviceClient = new WebserviceHttpClient($client, $username, $password);
+        $annotationReader = new AnnotationReader();
+        $classMetadataFactory = new ClassMetadataFactory(new AnnotationLoader($annotationReader));
+        $nameConverter = new MetadataAwareNameConverter($classMetadataFactory);
+        $propertyTypeExtractor = new ReflectionExtractor();
+        $this->serializer = new Serializer(
+            [
+                new ObjectNormalizer($classMetadataFactory, $nameConverter, null, $propertyTypeExtractor),
+                new ArrayDenormalizer(),
+            ],
+            ['json' => new JsonEncoder()],
+        );
+        $this->client = $client;
+        $this->username = $username;
+        $this->password = $password;
     }
 
     /**
@@ -51,7 +84,7 @@ class AutometricaClient
      */
     public function getCatalog(): iterable
     {
-        return $this->webserviceClient->requestCollection(CatalogResponse::class, 'catalogo.php');
+        return $this->requestCollection(CatalogResponse::class, 'catalogo.php');
     }
 
     /**
@@ -68,7 +101,7 @@ class AutometricaClient
      */
     public function getPrices(string $brand, string $model, int $year, string $trim, int $mileage = 0): iterable
     {
-        return $this->webserviceClient->requestCollection(VehiclePricesResponse::class, 'lineal.php', 'POST', [
+        return $this->requestCollection(VehiclePricesResponse::class, 'lineal.php', 'POST', [
             'brand' => $brand,
             'subbrand' => $model,
             'year' => $year,
@@ -90,5 +123,86 @@ class AutometricaClient
         $psr18Client = new Psr18Client($httpClient);
 
         return new self($psr18Client, $username, $password);
+    }
+
+    /**
+     * @phpstan-template T
+     * @phpstan-template TResponse of CollectionResponseInterface<T>
+     * @phpstan-param class-string<TResponse> $responseClass
+     * @param string $responseClass
+     * @param string $endpoint
+     * @param string $method
+     * @phpstan-param array<string, mixed> $headers
+     * @param array $headers
+     * @phpstan-return iterable<T>
+     * @return iterable
+     * @throws ClientExceptionInterface
+     * @throws BadRequestHttpException
+     * @throws UnauthorizedHttpException
+     * @throws UnknownHttpException
+     */
+    private function requestCollection(
+        string $responseClass,
+        string $endpoint,
+        string $method = 'GET',
+        array $headers = []
+    ): iterable {
+        $normalizedHeaders = $this->normalizeHeaders($headers);
+
+        $request = new Request($method, self::BASE_URL . $endpoint, $normalizedHeaders);
+        $response = $this->client->sendRequest($request);
+        $statusCode = $response->getStatusCode();
+
+        // The status code 400 Bad Request should be 403 Forbidden, but that's how the Autometrica server use them.
+        if ($statusCode === 400) {
+            throw new BadRequestHttpException('The username or password is incorrect');
+        }
+        if ($statusCode === 401) {
+            throw new UnauthorizedHttpException('The username or password is required');
+        }
+
+        if ($statusCode < 200 || $statusCode > 299) {
+            throw new UnknownHttpException($response->getReasonPhrase());
+        }
+
+        $responseBody = $response->getBody()->getContents();
+        /** @phpstan-var TResponse $dataResponse */
+        $dataResponse = $this->serializer->deserialize($responseBody, $responseClass, 'json', [
+            // We need to disable the type enforcement to manage the unknown kmGroup 0 in some prices
+            // With the typed properties for PHP 7.4, this should not be a problem
+            'disable_type_enforcement' => true,
+        ]);
+
+        return $dataResponse->getData();
+    }
+
+    /**
+     * @phpstan-param array<string, mixed> $headers
+     * @param array $headers
+     * @phpstan-return array<string, string>
+     * @return array
+     */
+    private function normalizeHeaders(array $headers): array
+    {
+        $normalizedHeaders = [];
+
+        // The Autometrica Webservice does not understand the UTF-8 charset in the headers, so we need to convert the
+        // charset to ISO-8859-1.
+        foreach ($headers as $header => $value) {
+            if (is_array($value)) {
+                $normalizedValue = array_map('utf8_decode', $value);
+            } else {
+                $normalizedValue = utf8_decode($value);
+            }
+
+            $normalizedHeaders[$header] = $normalizedValue;
+        }
+
+        // Set default headers
+        $normalizedHeaders['Content-Type'] = 'application/json; charset=UTF-8';
+        $normalizedHeaders['Username'] = $this->username;
+        $normalizedHeaders['Password'] = $this->password;
+
+        return $normalizedHeaders;
     }
 }
